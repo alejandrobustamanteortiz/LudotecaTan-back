f586d4cb3f6c65d15c7eb55f107402afa4d1299f
bububuu <albusor@gmail.com>
2023-08-21 13:05:15 +0200

    first commit

---
+++ src/test/java/com/example/ludotecatanback/LudotecaTanBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ludotecatanback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LudotecaTanBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
