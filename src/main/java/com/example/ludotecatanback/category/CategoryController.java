72e9694cab111d2bdd9b767c19a9b972b05239a4
bububuu <albusor@gmail.com>
2023-08-27 18:40:19 +0200

    Category Unit and Integration test

--- src/main/java/com/example/ludotecatanback/category/CategoryController.java
+++ src/main/java/com/example/ludotecatanback/category/CategoryController.java
@@ -11,20 +11,21 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RestController
-@Tag(name = "Category", description = "API of Category")
-@RequestMapping(value = "/category")
-@CrossOrigin(origins = "*")
+@RestController // indica que esta clase es un controlador que maneja solicitudes HTTP y produce resultados JSON
+@Tag(name = "Category", description = "API of Category") //anotación de Swagger para categorizar y documentar endpoints
+@RequestMapping(value = "/category") //Define el mapeo de ruta base para todos los métodos dentro de este controlador
+@CrossOrigin(origins = "*") //Permite solicitudes desde cualquier origen
 public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
 
     @Autowired
-    ModelMapper mapper;
+    ModelMapper mapper; //herramienta para mapear objetos de una clase a otra de manera automatizada y configurada
 
     @Operation(summary = "Find", description = "Method that returns a list of Categories")
     @RequestMapping(path = "", method = RequestMethod.GET)
+    @GetMapping("")
     public List<CategoryDto> findAll() {
 
         List<Category> categories = this.categoryService.findAll();
--- src/main/java/com/example/ludotecatanback/category/CategoryServiceImpl.java
+++ src/main/java/com/example/ludotecatanback/category/CategoryServiceImpl.java
@@ -4,11 +4,8 @@ import com.example.ludotecatanback.category.model.Category;
 import com.example.ludotecatanback.category.model.CategoryDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
